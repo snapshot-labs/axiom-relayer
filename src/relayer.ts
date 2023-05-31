19e19d7b4dd7dd2aa890b364f32a3ae196886c04
less <fabien@bonustrack.co>
2023-05-31 21:55:00 +0700

    initial commit

---
+++ src/relayer.ts
@@ -0,0 +1,98 @@
+import snapshot from '@snapshot-labs/snapshot.js';
+import { EnumType } from 'json-to-graphql-query';
+
+const hubURL = process.env.HUB_URL || 'https://hub.snapshot.org';
+
+export let lastMci = 29915678;
+
+async function getLastMci() {
+  // @TODO store last MCI
+  return lastMci;
+}
+
+async function getNextMessages(mci: number) {
+  const query = {
+    messages: {
+      __args: {
+        first: 10,
+        where: {
+          type_in: ['vote', 'proposal'],
+          mci_gt: mci
+        },
+        orderBy: 'mci',
+        orderDirection: new EnumType('asc')
+      },
+      mci: true,
+      id: true,
+      ipfs: true,
+      type: true,
+      timestamp: true,
+      space: true
+    }
+  };
+
+  try {
+    const results = await snapshot.utils.subgraphRequest(`${hubURL}/graphql`, query);
+    return results.messages;
+  } catch (e) {
+    console.error('failed to load messages', e);
+    return;
+  }
+}
+
+async function updateLastMci(mci: number) {
+  // @TODO update last MCI
+  lastMci = mci;
+}
+
+async function processMessages(messages: any[]) {
+  let lastMessageMci = null;
+
+  for (const message of messages) {
+    try {
+      if (message.type === 'proposal' && message.space === 'snapback.eth') {
+        // @TODO send proposal
+        console.log(`new proposal from ${message.address}`);
+      }
+
+      if (message.type === 'vote' && message.space === 'snapback.eth') {
+        // @TODO send vote
+        console.log(`new vote on snapback.eth from ${message.address}`);
+      }
+      lastMessageMci = message.mci;
+    } catch (error) {
+      console.error('failed to process message', message.id, error);
+      break;
+    }
+  }
+
+  if (lastMessageMci !== null) {
+    // Store latest message MCI
+    await updateLastMci(lastMessageMci);
+
+    console.log(`updated to MCI ${lastMessageMci}`);
+  }
+
+  return;
+}
+
+async function run() {
+  // Check latest indexed MCI from db
+  const lastMci = await getLastMci();
+
+  console.log(`last MCI ${lastMci}`);
+
+  // Load next messages after latest indexed MCI
+  const messages = await getNextMessages(lastMci);
+
+  if (messages && messages.length > 0) {
+    await processMessages(messages);
+  }
+
+  // Run again after 10sec
+  await snapshot.utils.sleep(5e3);
+
+  return run();
+}
+
+run();
